972a32105434f592a8f037bc17cf951ff9cee6f1
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-11-06 12:54:40 +0530

    PostfixEval.c

--- PostfixEval.c
+++ PostfixEval.c
@@ -6,79 +6,65 @@
 
 int stack[MAX];
 int top = -1;
-int a;
-char c;
 
 void push(int value) {
     top++;
     stack[top] = value;
- 
 }
 
 int pop() {
-    a = stack[top];
+    int a = stack[top];
     top--;
     return a;
 }
 
 int operand(char c) { 
-
-    if((c>='a' && c<='z') || (c>='A' && c<='Z')){
-    return 1;
-}
-else {
-    return 0;
-}
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
 
 int main() {
-    char postfix[MAX];
-    int i=0, o1, o2, r, a;
+    char postfix[MAX], c;
+    int i = 0, o1, o2, r, a;
     
     printf("Enter the postfix expression: ");
     scanf("%s", postfix);
     
-    while(postfix[i]!='\0') {
+    while (postfix[i] != '\0') {
         c = postfix[i];
-        if(operand(c)) {
-            printf("Enter the value of %c :", c);
+        if (operand(c)) {
+            printf("Enter the value of %c: ", c);
             scanf("%d", &a);
             push(a);
+        } else {
             o2 = pop();
             o1 = pop();
-        } else {
-            switch(c) {
-                case '+' :
-                r = o1 + o2;
-                break;
-                
-                case '-' :
-                r = abs(o1 - o2);
-                break;
-                
-                case '*' :
-                r = o1 * o2;
-                break;
-                
-                case '/' :
-                r = o1 / o2;
-                break;
-                
-                case '^' :
-                r = (int)pow(o1, o2);
-                break;
-                
+            switch (c) {
+                case '+':
+                    r = o1 + o2;
+                    break;
+                case '-':
+                    r = abs(o1 - o2);  // Use abs() if only positive results are desired
+                    break;
+                case '*':
+                    r = o1 * o2;
+                    break;
+                case '/':
+                    r = o1 / o2;
+                    break;
+                case '^':
+                    r = (int)pow(o1, o2);
+                    break;
                 default:
-                printf("Unknown operator encountered!");
-                return -1;
-                
-                
+                    printf("Unknown operator encountered!\n");
+                    return -1;
             }
             push(r);
-        }i++; 
+        }
+        i++;
     }
+    
     r = pop();
-    printf("POSTFIX EVALUATION: %d ", r);
+    printf("POSTFIX EVALUATION: %d\n", r);
    
     return 0;
 }
